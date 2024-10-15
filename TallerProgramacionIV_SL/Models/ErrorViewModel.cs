15b44eaf717064db4021e763771f26d03518a106
mango2309 <sebastian.largo@udla.edu.ec>
2024-10-15 18:19:55 -0500

    Agregar archivos de proyecto.

---
+++ TallerProgramacionIV_SL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerProgramacionIV_SL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
